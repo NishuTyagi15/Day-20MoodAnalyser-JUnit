c14f570724b7c1f392e47fc11f7f6ce2c46d6d1b
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-14 21:42:49 +0530

    Added:Sad Mood Analyser Program

---
+++ main/java/com/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,8 @@
+package com.moodanalyser;
+
+public class MoodAnalyser {
+
+    public String analyseMood(String message) {
+            return "SAD";
+    }
+}
--- test/java/com/moodanalyser/MoodAnalyserTest.java
+++ test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -1,7 +1,14 @@
 package com.moodanalyser;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MoodAnalyserTest {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Mood Analyser Problem!");
+
+    @Test
+    public void givenMessageWhenSadShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a Sad Message");
+        Assertions.assertEquals("SAD", mood);
     }
 }
